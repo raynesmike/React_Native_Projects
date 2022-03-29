940599d0ff0858efda04a70042cae1f59c5a03e3
Michael Raynes <raynesmike@yahoo.com>
2022-03-28 22:06:39 -0700

    michael/homework7 AsyncStorage

--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -1,83 +1,65 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
-import {AuthContext} from './context';
+import React, {useState, useEffect} from 'react';
+import {Button, Text, View} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export const Dashboard = () => {
-  return <Text>hello</Text>;
+export const DashboardScreen = () => {
+  const [myAuthToken, setMyAuthToken] = useState();
+  useEffect(() => {
+    getToken();
+  }, []);
+
+  const getToken = async () => {
+    try {
+      const value = await AsyncStorage.getItem('authKey');
+      if (value !== null) {
+        setMyAuthToken(value);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onLogin = async () => {
+    try {
+      setMyAuthToken('This is my auth');
+      await AsyncStorage.setItem('authKey', 'This is my auth');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onLogout = async () => {
+    try {
+      const value = await AsyncStorage.removeItem('authKey');
+      setMyAuthToken('');
+      if (value !== null) {
+        console.log(value);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={{justifyContent: 'center'}}>
+      {!myAuthToken ? (
+        <>
+          <Text>Sign in Page</Text>
+          <Button
+            title="Login"
+            onPress={() => {
+              this.onLogin();
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Text>Dashboard Page</Text>
+          <Button title="Logout" onPress={() => this.onLogout()} />
+        </>
+      )}
+
+      <Text>{JSON.stringify(myAuthToken)}</Text>
+    </View>
+  );
 };
-
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginVertical: 10,
-    borderRadius: 5,
-  },
-};
-
-const ScreenContainer = ({children}) => (
-  <View style={styles.container}>{children}</View>
-);
-
-export const Home = () => (
-  <ScreenContainer>
-    <Text>Master List Screen</Text>
-    <Button title="React Native by Example" onPress={alert('todo')} />
-    <Button title="React Native School" onPress={alert('todo')} />
-    <Button title="Drawer" onPress={alert('todo')} />
-  </ScreenContainer>
-);
-
-export const Details = () => (
-  <ScreenContainer>
-    <Text>Details Screen</Text>
-  </ScreenContainer>
-);
-
-export const Screen = () => (
-  <ScreenContainer>
-    <Text>Search Screen</Text>
-    <Button title="Search2" onPress={alert('todo')} />
-    <Button title="React Native School" onPress={alert('todo')} />
-  </ScreenContainer>
-);
-
-export const Screen2 = () => (
-  <ScreenContainer>
-    <Text>Search2 Screen</Text>
-  </ScreenContainer>
-);
-
-export const Profile = () => (
-  <ScreenContainer>
-    <Text>Profile Screen</Text>
-    <Button title="Drawer" onPress={alert('todo')} />
-    <Button title="Sign Out" onPress={alert('todo')} />
-  </ScreenContainer>
-);
-
-export const Splash = () => (
-  <ScreenContainer>
-    <Text>Loading...</Text>
-  </ScreenContainer>
-);
-
-export const SignIn = () => (
-  <ScreenContainer>
-    <Text>Sign In Screen</Text>
-    <Button title="Sign In" onPress={alert('todo')} />
-    <Button title="Create Account" onPress={alert('todo')} />
-  </ScreenContainer>
-);
-
-export const CreateAccount = () => (
-  <ScreenContainer>
-    <Text>Create Account Screen</Text>
-    <Button title="Sign Up" onPress={alert('todo')} />
-  </ScreenContainer>
-);
