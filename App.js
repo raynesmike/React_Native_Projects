35ee1fbcdf277e5952581ac58cfba79c93df1096
Michael Raynes <raynesmike@yahoo.com>
2022-03-29 21:14:28 -0700

    Homework7 v.2

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Dashboard} from './src/screens/dashboard/DashboardScreen';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {SignIn, CreateAccount} from './src/screens/dashboard/DashboardScreen';
-const AuthStack = createStackNavigator();
+import React, {useState} from 'react';
+import {Button, Text, View, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import {DashboardScreen} from './src/screens/dashboard/DashboardScreen';
 
-export const App = () => (
-  <NavigationContainer>
-    <AuthStack.Navigator>
-      <AuthStack.Screen name="SignIn" component={SignIn} />
-      <AuthStack.Screen name="CreateAccocunt" component={CreateAccount} />
-    </AuthStack.Navigator>
-  </NavigationContainer>
-);
+export const App = () => <DashboardScreen></DashboardScreen>;
 
 export default App;
--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -2,64 +2,59 @@ import React, {useState, useEffect} from 'react';
 import {Button, Text, View} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
+const getToken = async setMyAuthToken => {
+  try {
+    const value = await AsyncStorage.getItem('authKey');
+    if (value !== null) {
+      setMyAuthToken(value);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const onLogin = async setMyAuthToken => {
+  try {
+    setMyAuthToken('This is my auth');
+    await AsyncStorage.setItem('authKey', 'This is my auth');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const onLogout = async setMyAuthToken => {
+  try {
+    const value = await AsyncStorage.removeItem('authKey');
+    setMyAuthToken();
+    if (value !== null) {
+      console.log(value);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const DashboardScreen = () => {
   const [myAuthToken, setMyAuthToken] = useState();
   useEffect(() => {
-    getToken();
+    getToken(setMyAuthToken);
   }, []);
-
-  const getToken = async () => {
-    try {
-      const value = await AsyncStorage.getItem('authKey');
-      if (value !== null) {
-        setMyAuthToken(value);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  onLogin = async () => {
-    try {
-      setMyAuthToken('This is my auth');
-      await AsyncStorage.setItem('authKey', 'This is my auth');
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  onLogout = async () => {
-    try {
-      const value = await AsyncStorage.removeItem('authKey');
-      setMyAuthToken('');
-      if (value !== null) {
-        console.log(value);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <View style={{justifyContent: 'center'}}>
-      {!myAuthToken ? (
+      {myAuthToken ? (
         <>
-          <Text>Sign in Page</Text>
-          <Button
-            title="Login"
-            onPress={() => {
-              this.onLogin();
-            }}
-          />
+          <Text>Dashboard Page</Text>
+          <Button title="Logout" onPress={() => onLogout(setMyAuthToken)} />
         </>
       ) : (
         <>
-          <Text>Dashboard Page</Text>
-          <Button title="Logout" onPress={() => this.onLogout()} />
+          <Text>Sign in Page</Text>
+          <Button title="Login" onPress={() => onLogin(setMyAuthToken)} />
         </>
       )}
 
-      <Text>{JSON.stringify(myAuthToken)}</Text>
+      <Text>{myAuthToken}</Text>
     </View>
   );
 };
